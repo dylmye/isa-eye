f179c526fe141f20a5c6717df826b8bf14b8b3b9
Dylan Myers <git-signed@dylmye.me>
2025-01-02 22:30:31 +0000

    start adding previous years

--- constants/rules.ts
+++ constants/rules.ts
@@ -16,4 +16,105 @@ export const rules: Ruleset<IsaTypeCodes>[] = [
       },
     ],
   },
+  {
+    name: "2023/2024",
+    overallAllowance: 20000,
+    groupAllowances: [
+      {
+        codes: ["LIFETIME_CASH", "LIFETIME_STOCKS_AND_SHARES"],
+        allowance: 4000,
+      },
+      {
+        codes: ["JUNIOR_CASH", "JUNIOR_STOCKS_AND_SHARES"],
+        allowance: 9000,
+      },
+    ],
+  },
+  {
+    name: "2022/2023",
+    overallAllowance: 20000,
+    groupAllowances: [
+      {
+        codes: ["LIFETIME_CASH", "LIFETIME_STOCKS_AND_SHARES"],
+        allowance: 4000,
+      },
+      {
+        codes: ["JUNIOR_CASH", "JUNIOR_STOCKS_AND_SHARES"],
+        allowance: 9000,
+      },
+    ],
+  },
+  {
+    name: "2021/2022",
+    overallAllowance: 20000,
+    groupAllowances: [
+      {
+        codes: ["LIFETIME_CASH", "LIFETIME_STOCKS_AND_SHARES"],
+        allowance: 4000,
+      },
+      {
+        codes: ["JUNIOR_CASH", "JUNIOR_STOCKS_AND_SHARES"],
+        allowance: 9000,
+      },
+    ],
+  },
+  {
+    name: "2020/2021",
+    overallAllowance: 20000,
+    groupAllowances: [
+      {
+        codes: ["LIFETIME_CASH", "LIFETIME_STOCKS_AND_SHARES"],
+        allowance: 4000,
+      },
+      {
+        codes: ["JUNIOR_CASH", "JUNIOR_STOCKS_AND_SHARES"],
+        allowance: 9000,
+      },
+    ],
+  },
+  {
+    name: "2019/2020",
+    overallAllowance: 20000,
+    groupAllowances: [
+      {
+        codes: ["LIFETIME_CASH", "LIFETIME_STOCKS_AND_SHARES"],
+        allowance: 4000,
+      },
+      {
+        codes: ["JUNIOR_CASH", "JUNIOR_STOCKS_AND_SHARES"],
+        allowance: 4368,
+      },
+      // @TODO: ADD HTB
+    ],
+  },
+  {
+    name: "2018/2019",
+    overallAllowance: 20000,
+    groupAllowances: [
+      {
+        codes: ["LIFETIME_CASH", "LIFETIME_STOCKS_AND_SHARES"],
+        allowance: 4000,
+      },
+      {
+        codes: ["JUNIOR_CASH", "JUNIOR_STOCKS_AND_SHARES"],
+        allowance: 4260,
+      },
+      // @TODO: ADD HTB
+    ],
+  },
+  {
+    name: "2017/2018",
+    overallAllowance: 20000,
+    groupAllowances: [
+      {
+        codes: ["LIFETIME_CASH", "LIFETIME_STOCKS_AND_SHARES"],
+        allowance: 4000,
+      },
+      {
+        codes: ["JUNIOR_CASH", "JUNIOR_STOCKS_AND_SHARES"],
+        allowance: 4128,
+      },
+      // @TODO: ADD HTB
+    ],
+  },
 ];
