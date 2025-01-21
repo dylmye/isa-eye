1b02207de329ad117799d5166da693bc22b69396
Dylan Myers <git-signed@dylmye.me>
2025-01-21 21:58:15 +0000

    remove template images, isa card wip

--- components/CardBase.tsx
+++ components/CardBase.tsx
@@ -1,26 +1,67 @@
 import { getCrossPlatformColour } from "@/hooks/getCrossPlatformColour";
+import { useColorScheme } from "@/hooks/useColorScheme";
 import { PropsWithChildren } from "react";
-import { DimensionValue, Platform, StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+import {
+  DimensionValue,
+  Platform,
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from "react-native";
 
 interface CardBaseProps {
-  style?: StyleProp<ViewStyle>
+  style?: StyleProp<ViewStyle>;
 }
 
-const CardBase = ({ children, style }: PropsWithChildren<CardBaseProps>) => (
-  <View style={[styles.container, style]}>
-    {children}
-  </View>
-)
+const CardBase = ({ children, style }: PropsWithChildren<CardBaseProps>) => {
+  const colourScheme = useColorScheme();
+
+  const backgroundColor =
+    colourScheme === "dark"
+      ? getCrossPlatformColour(
+          "secondarySystemBackground",
+          "@android:color/system_accent1_900",
+          "rgba(255, 255, 255, 0.15)"
+        )
+      : getCrossPlatformColour(
+          "secondarySystemBackground",
+          "@android:color/system_accent1_900",
+          "rgb(206, 206, 206)"
+        );
+
+  const schemeStyles =
+    colourScheme === "light"
+      ? {
+          borderColor: 'rgba(0, 0, 0, 0.04)',
+          borderWidth: 2,
+          backgroundColor: getCrossPlatformColour(
+            "secondarySystemBackground",
+            "@android:color/system_accent1_900",
+            "rgb(206, 206, 206)"
+          ),
+        }
+      : {
+        backgroundColor: getCrossPlatformColour(
+          "secondarySystemBackground",
+          "@android:color/system_accent1_900",
+          "rgba(255, 255, 255, 0.15)"
+        )
+      };
+
+  return (
+    <View
+      style={[styles.container, schemeStyles, style]}
+    >
+      {children}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     display: "flex",
     minHeight: 64,
-    backgroundColor: getCrossPlatformColour(
-      "secondarySystemBackground",
-      "@android:color/system_accent1_900",
-      "rgba(255, 255, 255, 0.15)"
-    ),
     ...(Platform.OS === "web" && { maxWidth: "50rem" as DimensionValue }),
     padding: 16,
     borderRadius: 8,
--- components/implementations/IsaCard/index.tsx
+++ components/implementations/IsaCard/index.tsx
@@ -1,15 +1,35 @@
-import Card from "@/components/Card";
 import CardBase from "@/components/CardBase";
 import ThemedText from "@/components/ThemedText";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
+import { Image } from "expo-image";
 
 const IsaCard = () => (
-  <CardBase style={{ display: 'flex', flexDirection: 'row' }}>
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <ThemedText>Nationwide Lifetime ISA</ThemedText>
+  <CardBase style={{ display: "flex", flexDirection: "row" }}>
+    <View style={{ flex: 1, alignItems: "center", flexDirection: "row" }}>
+      <Image
+        source={require("@/assets/images/nationwide.svg")}
+        contentFit="fill"
+        style={{
+          width: 28,
+          height: 28,
+          borderRadius: 1000,
+          borderWidth: 2,
+          borderColor: "rgba(0, 0, 0, 0.04)",
+        }}
+      />
+      <ThemedText
+        type="defaultSemiBold"
+        adjustsFontSizeToFit
+        style={{ fontSize: 24, marginHorizontal: 8 }}
+        numberOfLines={1}
+      >
+        Nationwide Lifetime ISA
+      </ThemedText>
     </View>
-    <View style={{ alignItems: 'flex-end' }}>
-      <ThemedText type="defaultSemiBold" style={{ fontSize: 24 }}>£2,400</ThemedText>
+    <View style={{ alignItems: "flex-end" }}>
+      <ThemedText type="defaultSemiBold" style={{ fontSize: 24 }}>
+        £2,400
+      </ThemedText>
       <ThemedText>remaining</ThemedText>
     </View>
   </CardBase>
