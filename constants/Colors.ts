b627d26f749f241b3e9c4e9e837bfecb55dbac8f
Dylan Myers <git-signed@dylmye.me>
2025-07-14 22:09:05 +0100

    green theme because money

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,19 +3,19 @@
  */
 export const NAV_THEME = {
   light: {
-    background: "hsl(0 0% 100%)", // background
-    border: "hsl(240 5.9% 90%)", // border
-    card: "hsl(0 0% 100%)", // card
-    notification: "hsl(0 84.2% 60.2%)", // destructive
-    primary: "hsl(240 5.9% 10%)", // primary
-    text: "hsl(240 10% 3.9%)", // foreground
+    background: "hsl(160 41% 100%)", // background
+    border: "hsl(160 9% 93%)", // border
+    card: "hsl(160 41% 100%)", // card
+    notification: "hsl(18 86% 23%)", // destructive
+    primary: "hsl(160 99% 38%)", // primary
+    text: "hsl(160 61% 3%)", // foreground
   },
   dark: {
-    background: "hsl(240 10% 3.9%)", // background
-    border: "hsl(240 3.7% 15.9%)", // border
-    card: "hsl(240 10% 3.9%)", // card
-    notification: "hsl(0 72% 51%)", // destructive
-    primary: "hsl(0 0% 98%)", // primary
-    text: "hsl(0 0% 98%)", // foreground
+    background: "hsl(160 33% 3%)", // background
+    border: "hsl(160 9% 14%)", // border
+    card: "hsl(160 33% 3%)", // card
+    notification: "hsl(18 86% 58%)", // destructive
+    primary: "hsl(160 99% 38%)", // primary
+    text: "hsl(160 32% 99%)", // foreground
   },
 };
