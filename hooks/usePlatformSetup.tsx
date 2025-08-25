57a6ebeca5f4ac085c22e5e538f9b5228bdb449d
Dylan Myers <git-signed@dylmye.me>
2025-08-25 17:58:10 +0100

    fix theming on compiled web, add commands for testing compiled web

--- hooks/usePlatformSetup.tsx
+++ hooks/usePlatformSetup.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Appearance, Platform } from "react-native";
+import { useColorScheme as useNativewindColorScheme } from "nativewind";
+
 import { setAndroidNavBar } from "@/utils/setAndroidNavBar";
 
 export const usePlatformSpecificSetup = Platform.select({
   web: useSetWebBackgroundClassName,
   android: useSetAndroidNavigationBar,
-  default: () => {},
+  default: () => { },
 });
 
 const useIsomorphicLayoutEffect =
@@ -14,13 +16,16 @@ const useIsomorphicLayoutEffect =
     : React.useLayoutEffect;
 
 function useSetWebBackgroundClassName() {
+  const { setColorScheme } = useNativewindColorScheme();
   useIsomorphicLayoutEffect(() => {
     // Adds the background color to the html element to prevent white background on overscroll.
     document.documentElement.classList.add("bg-background");
     if (Appearance.getColorScheme() === "dark") {
       document.documentElement.classList.add("dark");
+      setColorScheme("dark");
     } else {
       document.documentElement.classList.remove("dark");
+      setColorScheme("light");
     }
   }, [Appearance.getColorScheme()]);
 }
