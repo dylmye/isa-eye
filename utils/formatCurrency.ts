964b56052ca9066db46bb131e837ff6d382322cc
Dylan Myers <git-signed@dylmye.me>
2026-04-04 21:11:37 +0100

    fix product name logic, currency formatting annoyance, name alignment

--- utils/formatCurrency.ts
+++ utils/formatCurrency.ts
@@ -2,7 +2,5 @@ export const formatCurrency = (amount: number) =>
   new Intl.NumberFormat("en-GB", {
     style: "currency",
     currency: "GBP",
-    maximumFractionDigits: 2,
-    minimumFractionDigits: 0,
     trailingZeroDisplay: "stripIfInteger",
   }).format(amount);
